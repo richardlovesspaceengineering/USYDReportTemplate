debb820545c9be448eccb480c730c556788aa7ff
Richard Rutherford <rrut5116@uni.sydney.edu.au>
2023-08-28 19:49:23 +1000

    Refactored with scripts kept in separate folder.

---
+++ Code/main.m
@@ -0,0 +1,11 @@
+%% Preamble
+clear;
+clc;
+close all;
+format long;
+
+% Figure exporting? Only set to true when needing to generate the figures.
+exportYorN = true;
+
+% Load figure size arrays.
+customStartup(exportYorN, '../Figures/');
